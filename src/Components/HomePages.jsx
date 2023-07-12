a05ceaebd896c44b548e2ec48825ef4daeecc4f7
NapoTwiixe <julien.milants@gmail.com>
2023-07-12 19:44:29 +0200

    add img and start css

--- src/Components/HomePages.jsx
+++ src/Components/HomePages.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import DescriptionsProps from './Props/DescriptionsProps'
 import TitleProps from './Props/TitleProps'
+import btc from './img/Bitcoin.png'
+import eth from './img/eth.svg'
+import bnb from './img/bn.svg'
 
 const HomePages = () => {
   return (
     <div>
-        <TitleProps title={"Hello World"}/>
-      <DescriptionsProps description={"lorem"}/>
+      <section class="section4">
+        <div class="card-container">
+            <div class="card">
+              <img src={btc} alt="" />
+              <TitleProps title={"Hello Wolrd"}/>
+              <DescriptionsProps description={"Lorem"}/>
+            </div>
+            <div class="card">
+              <img src={eth} alt="" />
+              <TitleProps title={"Hello Wolrd"}/>
+              <DescriptionsProps description={"Lorem"}/>
+            </div>
+            <div class="card">
+              <img src={bnb} alt="" />
+              <TitleProps title={"Hello Wolrd"}/>
+              <DescriptionsProps description={"Lorem"}/>
+            </div>
+        </div>
+    </section>
     </div>
   )
 }
