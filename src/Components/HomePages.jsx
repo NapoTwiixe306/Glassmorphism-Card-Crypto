7e9019b52fda951b008ca7686bdf283fe7994aef
NapoTwiixe <julien.milants@gmail.com>
2023-07-13 21:38:31 +0200

    switch text in json, and change background-color to background-image

--- src/Components/HomePages.jsx
+++ src/Components/HomePages.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import DescriptionsProps from './Props/DescriptionsProps'
 import TitleProps from './Props/TitleProps'
+
+{/*IMAGES IMPORT*/}
 import btc from './img/Bitcoin.png'
 import eth from './img/eth.svg'
 import bnb from './img/bn.svg'
 
+{/*JSON IMPORT*/}
+import data from './Text.json'
 const HomePages = () => {
   return (
     <div>
@@ -12,18 +16,18 @@ const HomePages = () => {
         <div className="card-container">
             <div className="card">
               <img src={btc} alt="" />
-              <TitleProps title={"Hello Wolrd"}/>
-              <DescriptionsProps description={"Lorem"}/>
+              <TitleProps title={data.btc_title}/>
+              <DescriptionsProps description={data.btc_description.btc}/>
             </div>
             <div className="card">
               <img src={eth} alt="" />
-              <TitleProps title={"Hello Wolrd"}/>
-              <DescriptionsProps description={"Lorem"}/>
+              <TitleProps title={data.eth_title}/>
+              <DescriptionsProps description={data.eth_description.eth}/>
             </div>
             <div className="card">
               <img src={bnb} alt="" />
-              <TitleProps title={"Hello Wolrd"}/>
-              <DescriptionsProps description={"Lorem"}/>
+              <TitleProps title={data.bnb_title}/>
+              <DescriptionsProps description={data.bnb_description.bnb}/>
             </div>
         </div>
     </section>
